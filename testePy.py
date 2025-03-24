9fdab42d4e5a4f82d7e89e246d7e448c73e28aa5
EricFortesdaCosta <ericcostaf.w@gmail.com>
2025-03-24 13:53:45 -0300

    Terminado a versão do projeto para pré termino

--- testePy.py
+++
@@ -1,9 +0,0 @@
-import os
-from docx import Document
-import os
-file_path = "Src/CertGarantia.docx"
-
-if os.path.exists(file_path):
-    doc = Document(file_path)
-else:
-    print(f"Arquivo não encontrado: {file_path}")
